6e4882a689fd9001465e64ec0d11c70cacbe127f
MuhammadSiddiq <707myworld@gmail.com>
2020-12-06 17:01:18 +0500

    asd

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
-var sections = $__('section');
+var sections = $$_('section');
 
+console.log(sections)
 
 var logScrollEvent = debounce(function(evt) {
-  console.count(evt)
+  // console.count(evt)
 }, 1000)
 
 
 
 window.addEventListener('scroll', logScrollEvent);
+
+window.addEventListener('scroll', function(){
+  console.log(window.pageXOffset);
+})
