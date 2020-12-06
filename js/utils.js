750b2fdd2ef2da2f7d1edf6821f802358a33057b
MuhammadSiddiq <707myworld@gmail.com>
2020-12-06 14:33:12 +0500

    first commit

---
+++ js/utils.js
@@ -0,0 +1,32 @@
+var $_ = function (selector, node = document) {
+  return node.querySelector(selector);
+};
+
+var $$_ = function (selector, node = document) {
+  return node.querySelectorAll(selector);
+};
+
+var createElement = function (tagName, className, text) {
+  var element = document.createElement(tagName);
+  element.setAttribute('class', className);
+  if (text) {
+    element.textContent = text;
+  }
+
+  return element;
+};
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
